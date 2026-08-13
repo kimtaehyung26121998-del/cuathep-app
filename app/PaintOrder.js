227874a1dda07eab46aca6535f092f8f0ee32dc2
Nguyễn Tuấn Vũ <kimtaehyung26121998@gmail.com>
2026-08-13 21:30:56 +0700

    Fix missing useEffect import in paint order

--- app/PaintOrder.js
+++ app/PaintOrder.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { toPng } from "html-to-image";
 
 const employees = [
